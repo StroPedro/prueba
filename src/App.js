1dde098d48a1fd94e7184d12e3ff13ba28c753a3
pedro <pedronndong777@gmail.com>
2021-07-19 15:54:20 +0100

    hasrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch , Route, BrowserRouter ,  } from 'react-router-dom'
+import { Switch , Route, HashRouter ,  } from 'react-router-dom'
 
 import Page1  from "./pages/pages1";
 import Page2 from "./pages/pages2";
@@ -9,14 +9,14 @@ function App(){
   
   return(
 
-          <BrowserRouter>
+          <HashRouter>
                   <Switch>
                           <Route exact  path= "/prueba" component = {Homme}/>
                           <Route exact  path= "/prueba/page1" component = {Page1}/>
                           <Route exact  path = "/prueba/page2" component = {Page2}/>
                           <Route exact  path= "/prueba/page3" component = {Page3}/>                   
                   </Switch>
-          </BrowserRouter> 
+          </HashRouter> 
  
   )
  
